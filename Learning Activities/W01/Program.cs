b6c00510817b85d360851edd9dce9eef6dcc6e05
Bryan Wesley Santana <bryanwessantana@gmail.com>
2026-05-09 10:02:28 -0300

    W01 Learning Activity

---
+++ Learning Activities/W01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+class Program {
+    static void Main() {
+        List<int> numbers = new List<int>();
+        
+        Console.WriteLine("--- Capacity Test ---");
+        for (int i = 0; i < 10; i++) {
+            numbers.Add(i);
+            Console.WriteLine($"Size: {numbers.Count}, Capacity: {numbers.Capacity}");
+        }
+
+        Stopwatch sw = new Stopwatch();
+        
+        sw.Start();
+        numbers.Insert(0, 99);
+        sw.Stop();
+        Console.WriteLine($"\nInsert at index 0 took: {sw.ElapsedTicks} ticks");
+
+        sw.Restart();
+        numbers.Add(100);
+        sw.Stop();
+        Console.WriteLine($"Append to end took: {sw.ElapsedTicks} ticks");
+    }
+}
