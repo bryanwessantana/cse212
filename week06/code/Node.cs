acf67b38ff9ebb0955750ba51be9dcbb84fb407d
Bryan Wesley Santana <bryanwessantana@gmail.com>
2026-06-09 23:20:46 -0300

    W06 Activities Completed

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -11,11 +11,11 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
+        if (value == this.Data)
+            return; 
 
         if (value < Data)
         {
-            // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
@@ -23,7 +23,6 @@ public class Node
         }
         else
         {
-            // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
@@ -33,13 +32,24 @@ public class Node
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == this.Data)
+            return true;
+
+        if (value < Data)
+        {
+            return Left != null && Left.Contains(value);
+        }
+        else
+        {
+            return Right != null && Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = (Left != null) ? Left.GetHeight() : 0;
+        int rightHeight = (Right != null) ? Right.GetHeight() : 0;
+        
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
